ac96b73165d5d23ee957a1023bb404e641dafa57
miopanda <114249779+miopanda@users.noreply.github.com>
2023-04-28 16:54:01 +0900

    usersモデルバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { minimum: 6 }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龯々]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龯々]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth, presence: true
 end

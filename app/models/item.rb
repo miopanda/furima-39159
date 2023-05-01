1a568d4db43247e696a258b1e97f1ed2dbe77e86
miopanda <114249779+miopanda@users.noreply.github.com>
2023-05-01 14:51:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_info, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_delivery_id, presence: true
+  validates :price, presence: true
+  validates :user_id, presence: true
 end

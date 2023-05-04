9c2de87911a992c3112d83847b6342cfa628d1de
miopanda <114249779+miopanda@users.noreply.github.com>
2023-05-04 17:53:31 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, except: [:index, :show]
 
   def index
@@ -20,16 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
-    
+
   end
 
   def edit
-    @item = Item.find(params[:id])
+
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -52,9 +51,13 @@ class ItemsController < ApplicationController
     ).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def set_item
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id && @item.order == nil
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user_id 
+      # && @item.order == nil
       redirect_to action: :index
     end
   end

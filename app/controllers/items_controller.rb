46da39607978908964fe53edf1128d17a9258d95
miopanda <114249779+miopanda@users.noreply.github.com>
2023-05-04 20:41:50 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -37,12 +37,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in?
       @item.destroy
       redirect_to root_path
-    else
-      redirect_to new_user_session_path
-    end
   end
 
   private

11fba7d3329d6d43321c6b0b2ff8e1954dd873d3
miopanda <114249779+miopanda@users.noreply.github.com>
2023-05-06 17:00:24 +0900

    Formオブジェクトの作成とエラーメッセージの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,30 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new
+  end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    @item = Item.find(params[:item_id])
+    params.require(:order_shipping_address).permit(
+      :postal_code, 
+      :prefecture_id, 
+      :city, 
+      :address, 
+      :building, 
+      :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
---
+++ app/models/order_shipping_address.rb
@@ -0,0 +1,27 @@
+class OrderShippingAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A0\d{10,11}\z/, message: "is invalid. Input only number"}
+  end
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(
+      postal_code: postal_code, 
+      prefecture_id: prefecture_id, 
+      city: city, 
+      address: address, 
+      building: building, 
+      phone_number: phone_number, 
+      order_id: order.id
+    )
+  end
+end
